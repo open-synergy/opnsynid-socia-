47196e58eaab6f2e159022226a22da666f6428a6
Miftahussalam <miftah_salam@ymail.com>
2023-10-31 13:45:22 +0700

    [ADD] ssi_mail

---
+++ ssi_mail/wizard/mail_wizard_invite.py
@@ -0,0 +1,28 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class MailWizardInvite(models.TransientModel):
+    _inherit = 'mail.wizard.invite'
+
+    commercial_contact_id = fields.Many2one(
+        string="Commercial Contact",
+        comodel_name="res.partner",
+        domain=[
+            ("parent_id", "=", False),
+        ],
+        required=False,
+    )
+    contact_group_id = fields.Many2one(
+        comodel_name='contact_group',
+        string='Contact Group',
+        required=False)
+
+    @api.onchange('commercial_contact_id')
+    def onchange_commercial_contact_id(self):
+        self.contact_group_id = False
+
+
+    @api.onchange('contact_group_id')
+    def onchange_contact_group_id(self):
+        self.partner_ids = self.contact_group_id.contact_ids
