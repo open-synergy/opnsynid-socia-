af4332916251564934a5974a9eba83afa43eb889
mikevhe18 <viriyananda.michael@gmail.com>
2024-01-31 16:46:02 +0700

    QC

--- ssi_mail/wizard/mail_wizard_invite.py
+++ ssi_mail/wizard/mail_wizard_invite.py
@@ -1,9 +1,8 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models
 
 
 class MailWizardInvite(models.TransientModel):
-    _inherit = 'mail.wizard.invite'
+    _inherit = "mail.wizard.invite"
 
     commercial_contact_id = fields.Many2one(
         string="Commercial Contact",
@@ -14,15 +13,13 @@ class MailWizardInvite(models.TransientModel):
         required=False,
     )
     contact_group_id = fields.Many2one(
-        comodel_name='contact_group',
-        string='Contact Group',
-        required=False)
+        comodel_name="partner_contact_group", string="Contact Group", required=False
+    )
 
-    @api.onchange('commercial_contact_id')
+    @api.onchange("commercial_contact_id")
     def onchange_commercial_contact_id(self):
         self.contact_group_id = False
 
-
-    @api.onchange('contact_group_id')
+    @api.onchange("contact_group_id")
     def onchange_contact_group_id(self):
         self.partner_ids = self.contact_group_id.contact_ids
